f1057681a0f7ade6cde25d425d6e60f8a4d3448e
Maria Wong <maria@Marias-MacBook-Pro.local>
2019-08-21 12:21:50 -0400

    adds Jenfkinsfile

---
+++ invoice-service/src/main/java/com/evanco/invoiceservice/controller/InvoiceController.java
@@ -0,0 +1,58 @@
+package com.evanco.invoiceservice.controller;
+
+import com.evanco.invoiceservice.model.Invoice;
+import com.evanco.invoiceservice.model.InvoiceViewModel;
+import com.evanco.invoiceservice.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RefreshScope
+@RequestMapping("/invoices")
+public class InvoiceController {
+
+    @Autowired
+    InvoiceService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public InvoiceViewModel addInvoice(@RequestBody @Valid InvoiceViewModel ivm){
+        return service.addInvoice(ivm);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InvoiceViewModel> getAllInvoices(){
+        return service.getAllInvoices();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public InvoiceViewModel getInvoice(@PathVariable("id") int id){
+        return service.getInvoice(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateInvoice(@RequestBody @Valid InvoiceViewModel ivm, @PathVariable("id") int id){
+        service.updateInvoice(ivm, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteInvoice(@PathVariable("id") int id){
+        service.deleteInvoice(id);
+    }
+
+    @GetMapping("/customer/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<InvoiceViewModel> getInvoiceByCustomerId(@PathVariable("id") int id){
+        return service.getInvoiceByCustomerId(id);
+    }
+
+}
