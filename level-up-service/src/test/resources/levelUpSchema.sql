1cf37a92faafe52d16cba30db5bd325b3b021021
Maria Wong <maria@Marias-MacBook-Pro.local>
2019-08-22 10:48:34 -0400

    adds working level up service

--- level-up-service/src/test/resources/levelUpSchema.sql
+++ level-up-service/src/test/resources/levelUpSchema.sql
@@ -1,5 +1,5 @@
-create schema if not exists level_up;
-use level_up;
+create schema if not exists level_up_test;
+use level_up_test;
 
 create table if not exists level_up (
     level_up_id int(11) not null auto_increment primary key,
